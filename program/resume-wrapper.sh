0258b73a3da55cc718c6fdb37d493da243075cf7
Sargun <sargunbhatti27@gmail.com>
2023-10-15 00:06:00 -0400

    initial commit

---
+++ program/resume-wrapper.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+file_name='resume
+
+if [ ! -d 'cv/' ]; then
+    echo Please create some components before executing the function >&2 
+    exit 5
+fi
+
+if [ -f 'resume-template.tex' ]; then 
+    rm 
+
+./execute.sh
