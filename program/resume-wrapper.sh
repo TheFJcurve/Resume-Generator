b053092996a49f2900fbd3a85c5275835a843ec0
Sargun <sargunbhatti27@gmail.com>
2023-10-15 13:20:55 +0800

    updated resume-wrapper

--- program/resume-wrapper.sh
+++ program/resume-wrapper.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-file_name='resume
+file_name='resume-template.tex'
+component_dir='cv'
+font_size=11px
 
-if [ ! -d 'cv/' ]; then
+
+if [ ! -d ${component_dir} ]; then
     echo Please create some components before executing the function >&2 
     exit 5
 fi
 
-if [ -f 'resume-template.tex' ]; then 
-    rm 
+echo \\documentclass[${font_size}, letterpaper]{article} > ${file_name}
 
-./execute.sh
+# ./execute.sh
