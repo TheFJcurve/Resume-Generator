681c8999cbc767562034ef1efad770d98a1eef28
Sargun <sargunbhatti27@gmail.com>
2023-10-26 11:30:43 -0400

    added ability to add multiple items in component

--- program/code-files/component-in-LaTeX.sh
+++ program/code-files/component-in-LaTeX.sh
@@ -23,110 +23,146 @@
 ## Adds a file to ../resumes/${resume_name}/${resume_name}-components/${resume_name}-${component_name}.tex
 
 achievements() {
-    echo 'Competition Name: '
-    read competition
-    echo 'Location: '
-    read location
-    echo 'Year: '
-    read year
-    echo 'Description: '
-    read description
-    
-    
+    echo 'How many achievements would you like to list? '
+    read count
     output='\\textbf{Achievement} \n
 
             \\par\\noindent\\rule{\\textwidth}{0.2pt} \n
 
-            \\begin{itemize} \n
-                \\item {'${competition}' \\\ \n
-                        '${location}' \\hfill '${year}' \\\ \n 
-                        '${description}'} \n
-            \\end{itemize}'
+            \\begin{itemize} \n'
+
+    for ((i=1; i<=${count};i++)); do
+        echo 'Details of Achievement' ${i}
+        echo ''
+        echo 'Competition Name: '
+        read competition
+        echo 'Location: '
+        read location
+        echo 'Year: '
+        read year
+        echo 'Description: '
+        read description
+        output+='\\item {'${competition}' \\\ \n
+                '${location}' \\hfill '${year}' \\\ \n 
+                '${description}'} \n'
+    done
+    
+    output+='\\end{itemize}'
 }
 
 certificates() {
-    echo 'Certificate Name: '
-    read name
-    echo 'Issuing Authority: '
-    read auth
-    echo 'Year: '
-    read year
-    echo 'Brief Description: '
-    read desc
-
+    echo 'How many Certificates do you wish to add? '
+    read count 
     output='\\textbf{Certificates} \n
 
             \\par\\noindent\\rule{\\textwidth}{0.2pt} \n
 
+            \\begin{enumerate} \n'
 
-            \\begin{enumerate} \n
-                \\item  {'${name}' \\\ \n
-                        '${auth}' \\hfill '${year}' \\\ \n
-                        '${desc}'}\n
-            \\end{enumerate}'
+    for ((i=1;i<=${count};i++)); do
+        echo 'Details of Certificate' ${i}
+        echo ''
+        echo 'Certificate Name: '
+        read name
+        echo 'Issuing Authority: '
+        read auth
+        echo 'Year: '
+        read year
+        echo 'Brief Description: '
+        read desc
+
+        output+='\\item  {'${name}' \\\ \n
+                            '${auth}' \\hfill '${year}' \\\ \n
+                            '${desc}'} \n'
+    done
+
+    output+='\\end{enumerate}'
 }
 
 competitions() {
-    echo 'Competition Name: '
-    read name
-    echo 'Position: '
-    read position
-    echo 'A Brief Description: '
-    read desc
-
+    echo 'How many Competitions do you wish to add? '
+    read count
     output='\\textbf{Competitions} \n
 
             \\par\\noindent\\rule{\\textwidth}{0.2pt} \n
 
-            \\begin{enumerate} \n
-            \\item {'${name}' | '${position}' \\\ \n
-                '${desc}'} \n
-            \\end{enumerate}'
+            \\begin{enumerate} \n'
+
+    for ((i=1;i<=${count};i++)); do
+        echo 'Details of Competition' ${i}
+        echo ''
+        echo 'Competition Name: '
+        read name
+        echo 'Position: '
+        read position
+        echo 'A Brief Description: '
+        read desc
+        output+='\\item {'${name}' | '${position}' \\\ \n
+                    '${desc}'} \n'
+    done
+
+    output+='\\end{enumerate}'
 }
 
 education() {
-    echo 'University Name: '
-    read name
-    echo 'Graduation Year: '
-    read grad
-    echo 'Program: '
-    read program
-    echo 'Relavent Courses List: '
-    read relcourses
-
-
+    echo 'How many Education Institutes do you wish to add? '
+    read count
     output='\\textbf{Education} \n
 
-            \\par\\noindent\\rule{\\textwidth}{0.2pt} \n
+            \\par\\noindent\\rule{\\textwidth}{0.2pt} \n'
+    
+    
+    for ((i=1; i<=${count};i++)); do
+        echo 'Details for Institute' ${i}
+        echo ''
+        echo 'University Name: '
+        read name
+        echo 'Graduation Year: '
+        read grad
+        echo 'Program: '
+        read program
+        echo 'Relavent Courses List: '
+        read relcourses
 
 
-            \\textbf{'${name}'} \\hfill {'${grad}'} \\\ \n
-            '${program}' \\\ \n
-            {Relavent Courses} - {'${relcourses}'} \\\'
+        output='\\textbf{'${name}'} \\hfill {'${grad}'} \\\ \n
+                '${program}' \\\ \n
+                {Relavent Courses} - {'${relcourses}'} \\\'
+    done    
 }
 
 experience() {
-    echo 'Company Name: '
-    read name 
-    echo 'Position: '
-    read position
-    echo 'Location: '
-    read location 
-    echo 'Start Date: '
-    read sdate
-    echo 'End Date: '
-    read edate
-    echo 'Job Description: '
-    read desc
+    echo 'How many Experiences would you like to include? '
+    read count 
 
     output='\\textbf{Experience} \n
 
-            \\par\\noindent\\rule{\\textwidth}{0.2pt} \n
+            \\par\\noindent\\rule{\\textwidth}{0.2pt} \n'
 
-            {\\textbf{'${name}'}, \\textnormal{\\textit{'${position}'} | '${location}'}} \\hfill {'${sdate}' - '${edate}'} \n
-            {\\begin{itemize} \n
-                    \\item {'${desc}'} \n
-                \\end{itemize}}'
+    for ((i=1;i<=${count}; i++)); do
+        echo 'Details for Experience' ${i}
+        echo ''
+
+        echo 'Company Name: '
+        read name 
+        echo 'Position: '
+        read position
+        echo 'Location: '
+        read location 
+        echo 'Start Date: '
+        read sdate
+        echo 'End Date: '
+        read edate
+        echo 'Job Description: '
+        read desc
+
+        output='
+                {\\textbf{'${name}'}, \\textnormal{\\textit{'${position}'} | '${location}'}} \\hfill {'${sdate}' - '${edate}'} \n
+                {\\begin{itemize} \n
+                        \\item {'${desc}'} \n
+                    \\end{itemize}}'
+    done
+
 }
 
 heading() {
@@ -174,63 +210,83 @@ interests() {
 }
 
 languages() {
-    echo 'Language: '
-    read name 
-    echo 'Proficiency Level: '
-    read proficiency
+    echo 'How many Languages would you like to input? '
+    read count
 
     output='\\textbf{Competitions} \n
 
             \\par\\noindent\\rule{\\textwidth}{0.2pt} \n
 
+            \\begin{itemize} \n'
 
-            \\begin{itemize} \n
-                \\item {'${name}' \n
-                        '${proficiency}'} \n
-            \\end{itemize}'
+    for ((i=1;i<=${count};i++)); do
+        echo 'Details about Language' ${i}
+        echo ''
+        echo 'Language: '
+        read name 
+        echo 'Proficiency Level: '
+        read proficiency
+        output+='\\item {'${name}': '${proficiency}'} \n'
+    done
+
+    output+='\\end{itemize}''
 }
 
 projects () {
-    echo 'Project Name: '
-    read name
-    echo 'Organisation or Self: '
-    read under
-    echo 'Start Date: '
-    read sdate 
-    echo 'End Date (In case ongoing, leave blank): '
-    read edate
-    echo 'GitHub Url: '
-    read github
-    echo 'Description of Project: '
-    read description
+    echo 'How many projects would you like to include? '
+    read count
 
     output='\\textbf{Projects} \n
 
             \\par\\noindent\\rule{\\textwidth}{0.2pt} \n
 
-            \\begin{itemize}
-                \\item {\\textbf{'${name}'} \\hfill \\faGithub \\hspace{.5pt} \\href{'${github}'}{GitHub} \\\ \n
+            \\begin{itemize} \n'
+    
+    for ((i=1; i<=${count}; i++)); do
+        echo 'Details of Project' ${i}
+        echo ''
+        echo 'Project Name: '
+        read name
+        echo 'Organisation or Self: '
+        read under
+        echo 'Start Date: '
+        read sdate 
+        echo 'End Date (In case ongoing, leave blank): '
+        read edate
+        echo 'GitHub Url: '
+        read github
+        echo 'Description of Project: '
+        read description
+        output+='\\item {\\textbf{'${name}'} \\hfill \\faGithub \\hspace{.5pt} \\href{'${github}'}{GitHub} \\\ \n
                         '${under}' \\hfill {'${sdate}' - '${edate}'} \n
                         { \n
                             \\begin{itemize} \n 
                                 \\item {'${description}'} \n
                             \\end{itemize} \n
-                        }} \n 
-            \\end{itemize}'
+                        }} \n'
+    done 
+    
+    output+='\\end{itemize}'
 }
 
 skills() {
-    echo 'Category: '
-    read category
-    echo 'List of Skills: '
-    read skills
+    echo 'How many skill categories would you like to add? '
+    read count
 
     output='\\textbf{Skills} \n
 
-            \\par\\noindent\\rule{\\textwidth}{0.2pt} \n
+            \\par\\noindent\\rule{\\textwidth}{0.2pt} \n'
 
-            \\noindent \n
-            {\\textbf{'${category}'} : {'${skills}'}}'
+    for ((i=1; i<=${count}; i++)); do
+        echo 'Details about Skill Category' ${i}
+        echo ''
+        echo 'Category: '
+        read category
+        echo 'List of Skills: '
+        read skills
+        output+='\\noindent \n
+                {\\textbf{'${category}'} : {'${skills}'}} \n'
+    done
 }
 
 resume_name=$1
